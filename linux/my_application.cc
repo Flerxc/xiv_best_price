4d0d560551ab57805a655c36d02c4209693f625d
Francis Leroux-Contant <francis.lerouxc@gmail.com>
2024-08-14 20:24:30 -0400

    Rename app

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "xiv_best_price");
+    gtk_header_bar_set_title(header_bar, "XIV BEST PRICE");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "xiv_best_price");
+    gtk_window_set_title(window, "XIV BEST PRICE");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
